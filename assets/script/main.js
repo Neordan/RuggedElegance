d6ecff8bbcba7c79d4a3ec5b437bffc9843c1b35
Neordan <melhian.adam@gmail.com>
2024-01-15 11:34:35 +0100

    Ajout de la fonction getCard pour afficher les données de mon json

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,16 @@
-function init() {
-    
+function init(cart) {
+
 }
+
+
+
+/**
+ * Recupère en AJAX les données du panier
+ */
+async function getCard() {
+    let response = await fetch('/data/cart.json');
+    let cart = await response.json();
+    init(cart);
+}
+
+getCart();

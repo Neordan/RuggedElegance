da29c1de053386bc3da23057a46e96956f286e59
Neordan <melhian.adam@gmail.com>
2024-01-16 21:25:08 +0100

    Initialisation de la class Delivery avec la methode pour ajouter les options de livraison

---
+++ assets/script/classes/Delivery.js
@@ -0,0 +1,21 @@
+class Delivery {
+    deliveryOptions = [];
+    
+    // Index de l'option de livraison sélectionnée par défaut
+    selectedDeliveryIndex = 0;
+
+    constructor() {
+        // Ajout des options de livraison
+        this.addDeliveryOption('Relais colis', 5);
+        this.addDeliveryOption('Domicile', 12);
+        
+        // Option de livraison par défaut (index 0 pour "Relais Colis")
+        this.selectDeliveryOption(0);
+    }
+
+    // Ajout des option de livraison au tableau
+    addDeliveryOption(name, cost) {
+        this.deliveryOptions.push({ name, cost });
+    }
+
+}

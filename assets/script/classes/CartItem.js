a8ed5cb312e6e444d928dcd802ac0151e8cd2035
Neordan <melhian.adam@gmail.com>
2024-01-18 16:33:57 +0100

    Ajout de la classe pour créer une ligne dans le panier

---
+++ assets/script/classes/CartItem.js
@@ -0,0 +1,13 @@
+
+/**
+ * Classe qui sert à la création des lignes dans le localStorage
+ */
+class CartItem {
+    constructor(id, name, price, quantity, img) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.img = img;
+    }
+}

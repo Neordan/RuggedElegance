9fa2c0f09c9864a07f385d490c934a153d78ea81
Neordan <melhian.adam@gmail.com>
2024-01-21 19:00:23 +0100

    Ajout du code pour l'apparition du menu hamburger

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,16 +1,25 @@
-
-function init(cart) {
-    const delivery = new Delivery();
-    new Lines(cart.products, delivery);
+// Fonction d'initialisation principale
+function init() {
+    const productManager = new ProductManager();
+    productManager.loadProducts();
 }
 
-/**
- * Recupère en AJAX les données du panier
- */
-async function getCart() {
-    let response = await fetch('/data/data.json');
-    let cart = await response.json();
-    init(cart);
-}
+document.addEventListener('DOMContentLoaded', init);
 
-getCart();
+
+
+//Apparition du menu en version mobile
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.querySelector(".custom-navbar-toggler-icon");
+    const navbarList = document.querySelector(".menu-hamburger");
+
+    if (menuButton && navbarList) {
+        menuButton.addEventListener("click", () => {
+            // Utilisez la méthode toggle pour basculer la classe 'show'
+            navbarList.classList.toggle("show");
+
+        });
+    } else {
+        console.error("Le bouton du menu ou l'élément de la liste du menu n'a pas été trouvé.");
+    }
+});
